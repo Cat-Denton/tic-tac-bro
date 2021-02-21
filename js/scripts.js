bf69f1435c0e4c8f492cd07363ed7afb15f02c86
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-02-21 17:38:32 -0600

    update website to most current code
    Co-authored-by: Cat Denton <willwdenton@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,18 +15,23 @@ function Game() {
 }
 
 function attachPlayerGridListeners() {
+  let i = 1;
+  let y = 1;
+  let gridString = $("#" + i.toString() + y.toString() + "p1");
+  let boardString = $("#" + i.toString() + y.toString());
   for (i = 1; i <= 3; i++) {
-    $("#11p1").on("click", function() {
-      console.log("X")
-      $("#11p1").text("X");
-      $("#11").text("X");
-    });
+    console.log (gridString)
+    for (y = 1; y <= 3; y++) {
+      gridString.on("click", function() {
+        console.log("X")
+        gridString.text("X");
+        boardString.text("X");
+      });
+    }
   }
 }
 $(document).ready(function() {
   attachPlayerGridListeners();   
 });
   // Create 4 constructors
-//2) Space
-//3) Board will have different objects
 //4) Game
