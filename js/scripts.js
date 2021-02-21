108ac716affb0453138e41c6bf28f55c19fda52c
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-02-21 16:59:31 -0600

    add event listener to span 11p1 and span 11 witth an X
    Co-authored-by: Cat Denton <willwdenton@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ function Space(row,column,mark) {
   this.column = column;
   this.mark = mark;
 }
+
 function Board() {
   this.spaces = {};
 }
@@ -13,16 +14,22 @@ function Game() {
   this.games = {} ;
 }
 
+function attachPlayerGridListeners() {
+  $("#11p1").on("click", function() {
+    console.log("X")
+    $("#11p1").text("X");
+    $("#11").text("X");
+  });
+};
 
-
-
-
-
-
-
-
+$(document).ready(function(){
+  
+  attachPlayerGridListeners();
+  
+})
 // Notes of Project
 // Create 4 constructors
+// writing a prototype that uses the space object to fill a space? fill a space and then create a space object
 //1) Create a Player constructor  
 //2) Space
 //3) Board will have different objects
