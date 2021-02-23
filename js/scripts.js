c41c0a57db60bdfd175d89c18d7fa6f2624d43d7
Cat-Denton <willwdenton@gmail.com>
2021-02-22 20:26:05 -0800

    Add logic to attachPlayerGridListeners function such that it creates space objects and populates them to a board object as players click on spaces
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>
    :

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-function Player(name) {
+function Player(name,mark) {
   this.name = name;
+  this.mark = mark;
 }
+
 function Space(row,column,mark) {
   this.row = row;
   this.column = column;
@@ -9,30 +11,82 @@ function Space(row,column,mark) {
 
 function Board() {
   this.spaces = {};
+  this.currentId = 0;
 }
+
+Board.prototype.spaceId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Board.prototype.takeSpace = function(space) {
+  space.id = this.spaceId();
+  this.spaces[space.id] = space;
+}
+
+
+
 function Game() {
   this.games = {} ;
 }
 
-function attachPlayerGridListeners() {
+
+
+function attachPlayerGridListeners(board) {
   let i = 1;
   let y = 1;
+  
   
   
   for (i = 1; i <= 3; i++) {
     for (y = 1; y <= 3; y++) {
       let gridString = $("#" + i.toString() + y.toString() + "p1");
       let boardString = $("#" + i.toString() + y.toString());
-      console.log(gridString);
+      let row = i;
+      let column = y;
+      
       gridString.on("click", function() {
+        let space = new Space(row,column,"X");
+        board.takeSpace(space)
+
         gridString.text("X");
         boardString.text("X");
       });
     }
   }
+  for (i = 1; i <= 3; i++) {
+    for (y = 1; y <= 3; y++) {
+      let gridString = $("#" + i.toString() + y.toString() + "p2");
+      let boardString = $("#" + i.toString() + y.toString());
+
+      let row = i;
+      let column = y;
+      gridString.on("click", function() {
+        let space = new Space(row,column,"O");
+        board.takeSpace(space)
+
+        gridString.text("O");
+        boardString.text("O");
+      });
+    }
+  }
 }
+
+let board = new Board();
+
 $(document).ready(function() {
-  attachPlayerGridListeners();   
+
+  
+  attachPlayerGridListeners(board);   
+  
 });
-  // Create 4 constructors
+
+// Create 4 constructors
 //4) Game
+
+// Player one turn. Click space
+// What are the winning combos?
+//1) complete row 
+//2) complete column 
+//3) diagonal
+ 
